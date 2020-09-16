19877e959d92e0458c6f1661fde887077994e3d0
David Dales <david.c.dales@gmail.com>
2020-09-15 23:30:55 -0400

    maybe stoppropogation will work...

--- src/index.js
+++ src/index.js
@@ -163,40 +163,29 @@ var gimmeMoustache = {
     );
 
     // touch screens
-    this.canvas.addEventListener(
-      "touchmove",
-      (e) => {
-        var touch = e.touches[0];
-        var mouseEvent = new MouseEvent("mousemove", {
-          clientX: touch.clientX,
-          clientY: touch.clientY,
-        });
-        canvas.dispatchEvent(mouseEvent);
-      },
-      true
-    );
-    this.canvas.addEventListener(
-      "touchstart",
-      (e) => {
-        e.preventDefault();
-        mousePos = getTouchPos(canvas, e);
-        var touch = e.touches[0];
-        var mouseEvent = new MouseEvent("mousedown", {
-          clientX: touch.clientX,
-          clientY: touch.clientY,
-        });
-        canvas.dispatchEvent(mouseEvent);
-      },
-      true
-    );
-    this.canvas.addEventListener(
-      "touchend",
-      (e) => {
-        var mouseEvent = new MouseEvent("mouseup", {});
-        canvas.dispatchEvent(mouseEvent);
-      },
-      true
-    );
+    this.canvas.addEventListener("touchmove", (e) => {
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+      });
+      canvas.dispatchEvent(mouseEvent);
+    });
+    this.canvas.addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      mousePos = getTouchPos(canvas, e);
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+      });
+      canvas.dispatchEvent(mouseEvent);
+    });
+    this.canvas.addEventListener("touchend", (e) => {
+      var mouseEvent = new MouseEvent("mouseup", {});
+      canvas.dispatchEvent(mouseEvent);
+    });
 
     this.pencil.addEventListener("click", (e) => this.selectTool("pencil"));
     this.line.addEventListener("click", (e) => this.selectTool("line"));
