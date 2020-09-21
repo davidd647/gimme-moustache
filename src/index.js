ecbf9f089edb3ccdb251a1128c98b68ead318fd3
David Dales <david.c.dales@gmail.com>
2020-09-21 12:40:25 -0400

    oops, forgot to add 'target'?

--- src/index.js
+++ src/index.js
@@ -89,8 +89,8 @@ var gimmeMoustache = {
 
   getMousePositionOnCanvas(e) {
     console.log(e);
-    const clientX = e.offsetX || e.touches[0].clientX - e.offsetLeft;
-    const clientY = e.offsetY || e.touches[0].clientY - e.offsetTop;
+    const clientX = e.offsetX || e.touches[0].clientX - e.target.offsetLeft;
+    const clientY = e.offsetY || e.touches[0].clientY - e.target.offsetTop;
     const { offsetX, offsetY } = e;
     const canvasX = offsetX; // - offsetLeft;
     const canvasY = offsetY; // - offsetTop;
